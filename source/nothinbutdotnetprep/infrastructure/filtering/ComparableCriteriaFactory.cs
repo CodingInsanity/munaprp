0a4fa595ae5081039abbe94d5410f48188658f48
CodingInsanity <drone_evil@gmx.net>
2011-04-11 17:58:31 +0200

    Pushing new changes

--- source/nothinbutdotnetprep/infrastructure/filtering/ComparableCriteriaFactory.cs
+++ source/nothinbutdotnetprep/infrastructure/filtering/ComparableCriteriaFactory.cs
@@ -4,9 +4,16 @@ namespace nothinbutdotnetprep.infrastructure.filtering
 {
     public class ComparableCriteriaFactory<ItemToMatch,PropertyType> where PropertyType : IComparable<PropertyType>
     {
+        PropertyAccessor<ItemToMatch, PropertyType> accessor;
+
+        public ComparableCriteriaFactory(PropertyAccessor<ItemToMatch, PropertyType> accessor)
+        {
+            this.accessor = accessor;
+        }
+
         public Criteria<ItemToMatch> between(PropertyType start, PropertyType end)
         {
-            throw new NotImplementedException();
+            return new AnonymousCriteria<ItemToMatch>(x => accessor(x).CompareTo(start) >= 0 && accessor(x).CompareTo(end) <= 0);
         }
     }
 }
